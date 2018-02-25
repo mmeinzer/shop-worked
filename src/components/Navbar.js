0d6b137533e348797bd92b109c90fb3ce10252cc
Matt <meinzer.matthew@gmail.com>
2018-02-25 12:34:05 -0600

    small tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import Link from 'gatsby-link';
+import React from "react";
+import Link from "gatsby-link";
 
-import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
+import logo from "../img/logo.svg";
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,7 +9,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
           </figure>
         </Link>
       </div>
@@ -18,16 +17,14 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
-        <a className="navbar-item" href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate" target="_blank" rel="noopener noreferrer">
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+        <a
+          className="navbar-item"
+          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
       </div>
     </div>
   </nav>
