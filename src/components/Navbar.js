b325030e48c87e32732e4c1e6259c4e5a5cc7fe1
Matt <meinzer.matthew@gmail.com>
2018-02-25 19:38:42 -0600

    Changes logo to png, navbar about page link size, latest posts text size with inline styles and Bulma classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React from "react";
 import Link from "gatsby-link";
 
-import logo from "../img/logo.svg";
+import logo from "../img/shop-worked-logo.png";
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-transparent" style={{ marginTop: "1rem" }}>
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
+        <Link
+          to="/"
+          className="navbar-item"
+          style={{ paddingLeft: "0", marginRight: "1.5rem" }}
+        >
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+            <img
+              src={logo}
+              alt="Shop Worked Logo"
+              style={{ maxHeight: "none", width: "450px" }}
+            />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
+        <Link className="navbar-item is-size-3" to="/about">
           About
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
       </div>
     </div>
   </nav>
