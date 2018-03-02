82a24279fb052f64b810685c6c9afd0384049589
Matt <meinzer.matthew@gmail.com>
2018-03-01 21:26:16 -0600

    Tweak navbar and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,22 @@
 import React from "react";
 import Link from "gatsby-link";
 
-import logo from "../img/shop-worked-logo.png";
+import logo from "../img/logo.svg";
 
 const Navbar = () => (
   <nav className="navbar is-transparent" style={{ marginTop: "1rem" }}>
     <div className="container">
       <div className="navbar-brand">
-        <Link
-          to="/"
-          className="navbar-item"
-          style={{ paddingLeft: "0", marginRight: "1.5rem" }}
-        >
-          <figure className="image">
-            <img
-              src={logo}
-              alt="Shop Worked Logo"
-              style={{ maxHeight: "none", width: "450px" }}
-            />
-          </figure>
+        <Link to="/" className="navbar-item">
+          <img
+            src={logo}
+            alt="Shop Worked Logo"
+            style={{ maxHeight: "10vh", width: "auto" }}
+          />
         </Link>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item is-size-3" to="/about">
+      <div className="navbar-end">
+        <Link className="navbar-item" to="/about">
           About
         </Link>
       </div>
